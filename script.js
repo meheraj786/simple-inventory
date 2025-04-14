d219df423e65455bbe5579a07bf60dcbbf8994f7
Mehraj Hosen <meherajhosen786@gmail.com>
2025-04-14 19:08:18 +0600

    trash icon add

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ const customerNameInput = document.querySelector('#customer-name');
 const incomeProductInput = document.querySelector('#income-product-name');
 const incomePriceInput = document.querySelector('#income-price');
 const incomeQuantityInput = document.querySelector('#income-quantity');
+const trashIcon= '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none"><path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="currentColor" d="M20 5a1 1 0 1 1 0 2h-1l-.003.071l-.933 13.071A2 2 0 0 1 16.069 22H7.93a2 2 0 0 1-1.995-1.858l-.933-13.07L5 7H4a1 1 0 0 1 0-2zm-3.003 2H7.003l.928 13h8.138zM14 2a1 1 0 1 1 0 2h-4a1 1 0 0 1 0-2z"/></g></svg>'
 
 class Product {
   constructor(name, price, quantity) {
@@ -61,7 +62,7 @@ class ProductManager {
     productList.innerHTML = '';
     this.products.forEach(product => {
       const productItem = document.createElement('tr');
-      productItem.innerHTML = `<td>${product.name}</td><td>$${product.price}</td><td>${product.quantity}</td><button onclick="delet(this)">Delete</button>`;
+      productItem.innerHTML = `<td>${product.name}</td><td>$${product.price}</td><td>${product.quantity}</td><button onclick="delet(this)">${trashIcon}</button>`;
       productList.appendChild(productItem);
     });
     document.querySelector('.products-number').textContent = this.getProductsNumber();
@@ -92,7 +93,7 @@ class CustomerManager {
     customerList.innerHTML = '';
     this.customers.forEach(customer => {
       const customerItem = document.createElement('tr');
-      customerItem.innerHTML = `<td>${customer.name}</td><td>${customer.company}</td><td>${customer.email}</td><td>${customer.phone}</td><button onclick="delet(this)">Delete</button>`;
+      customerItem.innerHTML = `<td>${customer.name}</td><td>${customer.company}</td><td>${customer.email}</td><td>${customer.phone}</td><button onclick="delet(this)">${trashIcon}</button>`;
       customerList.appendChild(customerItem);
     });
     document.querySelector('.customers-number').textContent = this.getCustomersNumber();
@@ -135,7 +136,7 @@ class IncomeManager {
       const incomeItem = document.createElement('tr');
       const total = income.getTotal();
       totalIncome += total;
-      incomeItem.innerHTML = `<td>${income.product}</td><td>$${income.price}</td><td>${income.quantity}</td><td>$${total}</td><button onclick="delet(this)">Delete</button>`;
+      incomeItem.innerHTML = `<td>${income.product}</td><td>$${income.price}</td><td>${income.quantity}</td><td>$${total}</td><button onclick="delet(this)">${trashIcon}</button>`;
       incomeList.appendChild(incomeItem);
     });
     document.querySelector('.income').textContent = `$${totalIncome}`;
